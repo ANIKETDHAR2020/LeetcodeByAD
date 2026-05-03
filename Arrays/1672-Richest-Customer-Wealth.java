62b89f5104eed718162550a0bc0677eabf05da54
aniket__ <138686800+ANIKETDHAR2020@users.noreply.github.com>
2026-05-03 23:56:19 +0530

    Update 1672-Richest-Customer-Wealth.java

--- Arrays/1672-Richest-Customer-Wealth.java
+++ Arrays/1672-Richest-Customer-Wealth.java
@@ -1,1 +1,24 @@
+//package com.ad;
+
+public class sumofrow {
+    public static void main(String[] args) {
+        int [] [] accounts = {{1,2,3},{3,2,1}};
+
+        System.out.println(sum1(accounts));
+    }
+    static int sum1 (int[][] accounts){
+        int max =0;
+        for(int row =0 ; row <accounts.length; row++ ){
+            int sum =0;
+            for(int column=0 ; column< accounts[row].length;column ++){
+                sum= sum+accounts[row][column];
+
+                }if(sum >max)
+                {
+                    max = sum;
+                }
+
+        } return max;
+    }
+}
 
